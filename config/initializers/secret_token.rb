3bc3794f28982f6eb8569f106602a29b3f00e467
saasbook <saasbook@saasbook.(none)>
2015-11-19 07:16:45 -0400

    model finished

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogLab61::Application.config.secret_token = '71b376276ae5eaee4f3603ac9b9066c8f41eef7587f0f27168104a2586b55595f9621234f020488d4d0f77f4769746fee6c185625d7380deb2da8789076d3e09'
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,47 @@
+=begin
+require 'rails_helper'
+
+RSpec.describe Post, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+=end
+
+=begin
+require 'rails_helper'
+RSpec.describe Post, type: :model do
+
+before(:all) do
+@post = Post.new(title: 'My Title', content: 'My content')
+end
+
+it 'should have a matching content' do
+expect(@post.content).to eq('My content')
+end
+
+it "should have a matching title" do
+expect(@post.title).to eq('My Title')
+end
+
+end
+=end
+
+require 'rails_helper'
+RSpec.describe Post, type: :controller do
+
+context '#index' do
+pending "Your code here"
+end
+
+context '#new' do
+pending "Your code here"
+end
+
+context '#create' do
+pending "Your code here"
+end
+
+context '#destroy' do
+pending "Your code here"
+end
+
+end
